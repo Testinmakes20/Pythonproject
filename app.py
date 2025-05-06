70d95435070c6df5dc467e395aa71ed1058a197f
Pooja <145401427+Testinmakes20@users.noreply.github.com>
2025-05-06 23:48:56 +0200

    Update app.py

--- app.py
+++ app.py
@@ -45,7 +45,9 @@ def delete(todo_id):
     db.session.commit()
     return redirect(url_for("home"))
 
+
 if __name__ == "__main__":
-  with app.app_context():
-     db.create_all()
-  app.run(debug=True)
+    with app.app_context():
+        db.create_all()
+    app.run(debug=True)
+
