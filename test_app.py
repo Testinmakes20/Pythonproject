a54fe18eee4afc93d122e78e98da42e57bcb93ce
Pooja <145401427+Testinmakes20@users.noreply.github.com>
2025-05-07 23:27:06 +0200

    Update test_app.py

--- test_app.py
+++ test_app.py
@@ -6,14 +6,14 @@ from app import app, db
 def client():
     app.config["TESTING"] = True
     app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///:memory:"  # Use in-memory DB for testing
-    
+
     # Create tables before tests run
     with app.app_context():
         db.create_all()
-    
+
     with app.test_client() as client:
         yield client
-    
+
     # Optionally drop tables after tests
     with app.app_context():
         db.drop_all()
